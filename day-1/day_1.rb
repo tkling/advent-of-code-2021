3067de09764d68886c5a97904241eb5ddd21cdab
tkling <tyling@gmail.com>
2021-12-03 10:04:33 -0500

    hooray day 1 complete ⭐

--- day-1/day_1.rb
+++ day-1/day_1.rb
@@ -3,8 +3,15 @@
 input = File.read('day_1_input.txt').split("\n").map(&:to_i)
 
 depth_increases = input.each_with_index.filter_map do |depth, i|
-  depth if depth > input[i - 1]
+  depth if depth > input[i - 1] && i.positive?
+end
+
+triplets = input.each_cons(3).to_a
+tripled_sum_increases = triplets.each_with_index.filter_map do |triple, i|
+  sum = triple.sum
+  sum if sum > triplets[i - 1].sum && i.positive?
 end
 
 puts "total input size: #{input.size}"
 puts "number of depth increases: #{depth_increases.size}"
+puts "number of triplet sum increases: #{tripled_sum_increases.size}"
